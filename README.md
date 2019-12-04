bf335d5f81e64a5a4532fbe3c0c6d7ddb73ee381
LeifChen <leifchen90@gmail.com>
2019-12-04 16:01:02 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # springcloud-weather-demo
+
+基于 Spring Cloud 微服务架构实现天气预报服务。
+
+## 参考
+
+[从天气项目看Spring Cloud微服务治理](https://coding.imooc.com/class/chapter/177.html)
---
+++ src/main/java/com/chen/weather/App.java
@@ -0,0 +1,18 @@
+package com.chen.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App
+ * <p>
+ * @Author LeifChen
+ * @Date 2019-12-04
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
