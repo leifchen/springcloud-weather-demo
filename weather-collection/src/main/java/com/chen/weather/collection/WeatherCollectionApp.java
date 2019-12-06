125fc8663a957bfce3d90fe24ab4318ac0b1c0e2
LeifChen <leifchen90@gmail.com>
2019-12-06 17:20:10 +0800

    feat: add weather eureka client

--- weather-collection/src/main/java/com/chen/weather/collection/WeatherCollectionApp.java
+++ weather-collection/src/main/java/com/chen/weather/collection/WeatherCollectionApp.java
@@ -2,6 +2,7 @@ package com.chen.weather.collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @Author LeifChen
  * @Date 2019-12-06
  */
+@EnableDiscoveryClient
 @EnableScheduling
 @SpringBootApplication
 public class WeatherCollectionApp {
--- weather-data/src/main/java/com/chen/weather/data/WeatherDataApp.java
+++ weather-data/src/main/java/com/chen/weather/data/WeatherDataApp.java
@@ -2,6 +2,7 @@ package com.chen.weather.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 天气数据API微服务
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Author LeifChen
  * @Date 2019-12-06
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WeatherDataApp {
 
