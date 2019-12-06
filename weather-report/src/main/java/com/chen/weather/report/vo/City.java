a0d590b6e6250857e93bc21ebb30151b42a0308a
LeifChen <leifchen90@gmail.com>
2019-12-06 16:04:48 +0800

    feat: add weather report (#5)

---
+++ weather-report/src/main/java/com/chen/weather/report/vo/City.java
@@ -0,0 +1,20 @@
+package com.chen.weather.report.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 城市
+ * <p>
+ * @Author LeifChen
+ * @Date 2019-12-05
+ */
+@Getter
+@Setter
+public class City {
+
+	private String cityId;
+	private String cityName;
+	private String cityCode;
+	private String province;
+}
