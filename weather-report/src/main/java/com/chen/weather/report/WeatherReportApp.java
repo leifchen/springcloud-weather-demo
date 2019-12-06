871ff2042b6536062403d7037ccd931311154dff
LeifChen <leifchen90@gmail.com>
2019-12-06 16:03:55 +0800

    feat: add weather report

---
+++ weather-report/src/main/java/com/chen/weather/report/WeatherReportApp.java
@@ -0,0 +1,18 @@
+package com.chen.weather.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 天气预报微服务
+ * <p>
+ * @Author LeifChen
+ * @Date 2019-12-06
+ */
+@SpringBootApplication
+public class WeatherReportApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherReportApp.class, args);
+    }
+}
---
+++ weather-report/src/main/java/com/chen/weather/report/controller/WeatherReportController.java
@@ -0,0 +1,52 @@
+package com.chen.weather.report.controller;
+
+import com.chen.weather.report.service.WeatherReportService;
+import com.chen.weather.report.vo.City;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * WeatherReportController
+ * <p>
+ * @Author LeifChen
+ * @Date 2019-12-05
+ */
+@Slf4j
+@RestController
+@RequestMapping("/report")
+public class WeatherReportController {
+
+    @Autowired
+    private WeatherReportService weatherReportService;
+
+    @GetMapping("/cityId/{cityId}")
+    public ModelAndView getReportByCityId(@PathVariable("cityId") String cityId, Model model) {
+        List<City> cityList = null;
+
+        try {
+            // todo 改为由城市数据 API 微服务来提供数据
+            cityList = new ArrayList<>();
+            City city = new City();
+            city.setCityId("101280601");
+            city.setCityName("深圳");
+            cityList.add(city);
+        } catch (Exception e) {
+            log.error("Exception!", e);
+        }
+
+        model.addAttribute("title", "天气预报");
+        model.addAttribute("cityId", cityId);
+        model.addAttribute("cityList", cityList);
+        model.addAttribute("report", weatherReportService.getDataByCityId(cityId));
+        return new ModelAndView("weather/report", "reportModel", model);
+    }
+}
